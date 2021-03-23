ae09af322ddac7942db3a871223ea946d7ca317d
aiwaverse <aiwavision@protonmail.com>
2021-03-23 18:49:27 -0300

    started the assigment

--- src/Automata.hs
+++ src/Automata.hs
@@ -1,15 +1,37 @@
-{- |
-Copyright: (c) 2021 phi
-SPDX-License-Identifier: MIT
-Maintainer: phi <aiwaverse@protonmail.com>
-
-See README for more info
--}
-
+-- |
+-- Copyright: (c) 2021 phi
+-- SPDX-License-Identifier: MIT
+-- Maintainer: phi <aiwaverse@protonmail.com>
+--
+-- See README for more info
+{-# LANGUAGE DerivingStrategies #-}
 module Automata
-    ( projectName
-    ) where
+  ( AutomataState (..)
+  , AFD (..)
+  , next
+  , projectName
+  ) where
 
+import qualified Data.Map  as Map
+
+import qualified Data.Text as T
 
 projectName :: String
 projectName = "automata"
+
+data AutomataState = AState { name           :: T.Text
+                            , initial        :: Bool
+                            , final          :: Bool
+                            , adjacentStates :: Map.Map T.Text T.Text
+                            }
+  deriving stock (Eq, Show)
+
+newtype AFD = AFD (Map.Map T.Text AutomataState)
+
+next :: AFD -> AutomataState -> T.Text -> Either T.Text AutomataState
+next (AFD m) st c = case adjacentNames of
+                     Nothing -> Left $ "Failed by indeterminate transiction at state " <> name st
+                     Just s -> let nextState = Map.lookup s m in
+                       maybe (Left "This really shouldn't happen, check the Automata") Right nextState
+  where
+    adjacentNames = Map.lookup c (adjacentStates st)
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,7 +1,19 @@
 module Main (main) where
 
-import Automata (projectName)
+import           Automata
+import qualified Data.Map   as Map
+import           Test.HUnit
 
+stateQ0 = AState "q0" True True (Map.fromList [("a", "q1"), ("b", "q0")])
+stateQ1 = AState "q1" False False (Map.fromList [("a", "q0"), ("b", "q1")])
+
+automataT = AFD $ Map.fromList [(name stateQ0, stateQ0), (name stateQ1, stateQ1)]
+
+test1 = TestCase $ assertEqual "a at q0"  (Right stateQ1) (next automataT stateQ0 "a")
+test2 = TestCase $ assertEqual "b at q0" (Right stateQ0) (next automataT stateQ0 "b")
+test3 = TestCase $ assertEqual "a at q1" (Right stateQ0) (next automataT stateQ1 "a")
+test4 = TestCase $ assertEqual "b at q1" (Right stateQ1) (next automataT stateQ1 "b")
+tests = TestList [test1, test2, test3, test4]
 
 main :: IO ()
-main = putStrLn ("Tests for " ++ projectName)
+main = runTestTTAndExit tests
