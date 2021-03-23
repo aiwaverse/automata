3f25ecb79403ee6a52fbfd37b7a0cbd3ac64f0d7
aiwaverse <aiwavision@protonmail.com>
2021-03-23 15:13:47 -0300

    Create the project

---
+++ src/Automata.hs
@@ -0,0 +1,15 @@
+{- |
+Copyright: (c) 2021 phi
+SPDX-License-Identifier: MIT
+Maintainer: phi <aiwaverse@protonmail.com>
+
+See README for more info
+-}
+
+module Automata
+    ( projectName
+    ) where
+
+
+projectName :: String
+projectName = "automata"
