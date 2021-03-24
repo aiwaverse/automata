7d71645a744365157b077d19b62a0e563218dd50
aiwaverse <aiwavision@protonmail.com>
2021-03-24 13:25:32 -0300

    Finished validate

--- src/Automata.hs
+++ src/Automata.hs
@@ -10,6 +10,7 @@ module Automata
   , AFD(..)
   , next
   , getInitial
+  , validate
   , projectName
   ) where
 
@@ -17,7 +18,6 @@ import qualified Data.Map  as Map
 
 import qualified Data.Text as T
 
-import Data.List(foldl')
 
 projectName :: String
 projectName = "automata"
@@ -26,7 +26,7 @@ projectName = "automata"
 data AutomataState = AState { name           :: T.Text
                             , initial        :: Bool
                             , final          :: Bool
-                            , adjacentStates :: Map.Map T.Text T.Text
+                            , adjacentStates :: Map.Map Char T.Text
                             }
   deriving stock (Eq, Show)
 
@@ -34,7 +34,7 @@ data AutomataState = AState { name           :: T.Text
 newtype AFD = AFD (Map.Map T.Text AutomataState)
 
 -- | Given the automata, the current state @st and a text @c, returns the next state, if available
-next :: AFD -> AutomataState -> T.Text -> Either T.Text AutomataState
+next :: AFD -> AutomataState -> Char -> Either T.Text AutomataState
 next (AFD m) st c = case adjacentNames of
   Nothing -> Left $ "Failed by indeterminate transiction at state " <> name st
   Just s ->
@@ -42,18 +42,20 @@ next (AFD m) st c = case adjacentNames of
     in  maybe (Left "This really shouldn't happen, check the Automata") Right nextState
   where adjacentNames = Map.lookup c (adjacentStates st)
 
-getInitial :: AFD -> Maybe AutomataState
+getInitial :: AFD -> AutomataState
 getInitial (AFD m) = case Map.toList $ Map.filter initial m of
-  []  -> Nothing
-  [x] -> Just $ snd x
-  _   -> Nothing
+  []  -> error "No initial state on the Automata"
+  [x] -> snd x
+  _   -> error "More than one initial state on the Automata"
 
 validate :: AFD -> T.Text -> Either T.Text Bool
-validate automata w = undefined
-  where
-    initialState = getInitial automata
-    go :: AutomataState -> T.Text -> Either T.Text Bool
-    go currState (T.null -> True) = Right $ final currState
-    go currState wordToTest = case next automata currState (T.pack [T.head wordToTest]) of
-                                  Left t -> Left t
-                                  Right s -> go s (T.tail wordToTest)
+validate automata w = case go initialState w of
+  Right b -> if b then Right b else Left "Rejected by not reacing a final state"
+  Left  t -> Left t
+ where
+  initialState = getInitial automata
+  go :: AutomataState -> T.Text -> Either T.Text Bool
+  go currState (T.null -> True) = Right $ final currState
+  go currState wordToTest       = case next automata currState (T.head wordToTest) of
+    Right s -> go s (T.tail wordToTest)
+    Left  s -> Left s
