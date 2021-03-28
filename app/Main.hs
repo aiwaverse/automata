0498afc386261474f90ea63016324753e5244b20
aiwaverse <aiwavision@protonmail.com>
2021-03-28 19:58:25 -0300

    Started creating the executable

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,27 @@
-module Main (main) where
+module Main
+  ( main
+  ) where
 
-import Automata (projectName)
+import           AFDCreator
+import           Automata
+import           FileParser
 
+import           Text.Megaparsec
+    (parseMaybe)
+
+import qualified Data.Text       as T
+    (pack, unwords)
+import qualified Data.Text.IO    as TIO
+
+import           Data.Maybe
+    (fromMaybe)
 
 main :: IO ()
-main = putStrLn ("Executable for " ++ projectName)
+main = do
+  TIO.putStrLn "Enter the string"
+  input <- TIO.getLine
+  let (aName, aStates, alphabet, iState, fStates, functionProgram) =
+        fromMaybe ("", [], [], "", [], []) (parseMaybe afdFileParser input)
+  let afd = makeAFD (aStates, iState, fStates, functionProgram)
+  TIO.putStrLn $ "Automata " <> aName <> " on alphabet " <> T.unwords alphabet
+  TIO.putStrLn $ T.pack . show $ afd
